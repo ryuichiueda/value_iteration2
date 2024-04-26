c64909605fec2cc40453d3199056b9745fa95e5c
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-04-26 09:28:15 +0900

    Add launch file

---
+++ launch/turtle.launch.py
@@ -0,0 +1,22 @@
+import launch, os
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from glob import glob
+
+
+def generate_launch_description():
+    config = os.path.join(
+      get_package_share_directory('value_iteration2'),
+      'config',
+      'params.yaml'
+    )
+
+    vi_node = Node(
+            package='value_iteration2',
+            namespace='value_iteration2',
+            executable='vi_node',
+            name='vi_node',
+            parameters=[config],
+        )
+
+    return launch.LaunchDescription([vi_node])
