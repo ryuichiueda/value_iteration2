7cbf5366fec9e9613e8e0bf93eb4af637216d1a0
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-04-26 13:16:18 +0900

    Add tb3 gazebo dir

--- launch/turtle.launch.py
+++ launch/turtle.launch.py
@@ -1,9 +1,12 @@
 import launch, os
 from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    tb3_gazebo_share_dir = get_package_share_directory("turtlebot3_gazebo")
+
     config = os.path.join(
       get_package_share_directory('value_iteration2'),
       'config',
