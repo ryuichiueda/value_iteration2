e94cc1de7a7306e0f3e6feb229e979cdc0adffd2
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-04-29 16:26:46 +0900

    Enable to launch vi_node

--- launch/turtle.launch.py
+++ launch/turtle.launch.py
@@ -9,16 +9,7 @@ from launch.actions import IncludeLaunchDescription
 
 
 def generate_launch_description():
-    tb3_gazebo_share_dir = get_package_share_directory("turtlebot3_gazebo")
     emcl2_share_dir = get_package_share_directory("emcl2")
-    rviz_config_dir = os.path.join(get_package_share_directory('value_iteration2'), 'rviz', 'config.rviz')
-
-    tb3_gazebo_launch = launch.actions.IncludeLaunchDescription(
-      PythonLaunchDescriptionSource([tb3_gazebo_share_dir + "/launch/turtlebot3_world.launch.py"]),
-    )
-    emcl2_launch = launch.actions.IncludeLaunchDescription(
-      PythonLaunchDescriptionSource([emcl2_share_dir + "/test/test.launch.xml"]),
-    )
 
     config = os.path.join(
       get_package_share_directory('value_iteration2'),
@@ -34,13 +25,7 @@ def generate_launch_description():
             parameters=[config],
         )
 
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        arguments=['-d', rviz_config_dir + '/config/config.rviz'])
-
-    launch_include = IncludeLaunchDescription(
+    emcl2_launch = IncludeLaunchDescription(
         XMLLaunchDescriptionSource(
             os.path.join(
                 get_package_share_directory("emcl2"),
@@ -50,9 +35,6 @@ def generate_launch_description():
     )
 
     return launch.LaunchDescription([
-        launch_include
-    #    tb3_gazebo_launch,
-        #emcl2_launch,
-    #    vi_node,
-    #    rviz_node,
+        emcl2_launch,
+        vi_node,
     ])
